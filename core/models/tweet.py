799ab6c8a4a65090b43736542fc37c847fc08113
Nufore <werd322@yandex.ru>
2024-03-17 20:23:15 +0300

    create user_relation mixin

--- core/models/tweet.py
+++ core/models/tweet.py
@@ -1,20 +1,21 @@
 from typing import TYPE_CHECKING
 
-from .base import Base
 from sqlalchemy import Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .user import User
 
 
-class Tweet(Base):
+class Tweet(UserRelationMixin, Base):
     __tablename__ = "tweets"
+    _user_back_populates = "tweets"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     content: Mapped[str] = mapped_column(Text, nullable=False)
 
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="tweets")
+    # user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    # user: Mapped["User"] = relationship(back_populates="tweets")
