ab96c2fea5289cb3038e68b787b2d8a7bdca2bc9
Nufore <werd322@yandex.ru>
2024-03-19 22:18:21 +0300

    реализованы post и delete методы для /tweets/{tweet_id}/likes

--- fastapi_app/tweets/crud.py
+++ fastapi_app/tweets/crud.py
@@ -1,8 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .schemas import TweetCreate
-from core.models import Tweet
+from core.models import Tweet, Like
 
 
 async def create_tweet(session: AsyncSession, tweet_in: TweetCreate):
@@ -16,7 +16,7 @@ async def get_tweet(session: AsyncSession, tweet_id: int) -> Tweet | None:
     return await session.get(Tweet, tweet_id)
 
 
-async def delete_tweet(session: AsyncSession, tweet_id, user_id):
+async def delete_tweet(session: AsyncSession, tweet_id: int, user_id: int):
     stmt = select(Tweet).where(Tweet.id == tweet_id, Tweet.user_id == user_id)
     tweet: Tweet | None = await session.scalar(stmt)
     if tweet:
@@ -24,3 +24,24 @@ async def delete_tweet(session: AsyncSession, tweet_id, user_id):
         await session.commit()
         return True
     return False
+
+
+async def create_like(session: AsyncSession, tweet_id: int, user_id: int):
+    stmt = select(Like).where(Like.user_id == user_id, Like.tweet_id == tweet_id)
+    is_exists: Like | None = await session.scalar(stmt)
+    if is_exists:
+        return {"result": False}
+    new_like = Like(user_id=user_id, tweet_id=tweet_id)
+    session.add(new_like)
+    await session.commit()
+    return {"result": True}
+
+
+async def delete_like(session: AsyncSession, tweet_id: int, user_id: int):
+    stmt = select(Like).where(Like.user_id == user_id, Like.tweet_id == tweet_id)
+    like: Like | None = await session.scalar(stmt)
+    if not like:
+        return {"result": False}
+    await session.delete(like)
+    await session.commit()
+    return {"result": True}
--- fastapi_app/tweets/views.py
+++ fastapi_app/tweets/views.py
@@ -33,3 +33,27 @@ async def delete_tweet(
     user = await users_crud.get_user_by_api_key(session=session, api_key=api_key)
     res = await crud.delete_tweet(session=session, tweet_id=tweet_id, user_id=user.id)
     return {"result": res}
+
+
+@router.post("/{tweet_id}/likes", status_code=status.HTTP_201_CREATED)
+async def create_like(
+    request: Request,
+    tweet_id: int,
+    tweet: Tweet = Depends(tweet_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    api_key = request.headers.get("Api-Key")
+    user = await users_crud.get_user_by_api_key(session=session, api_key=api_key)
+    return await crud.create_like(session=session, tweet_id=tweet_id, user_id=user.id)
+
+
+@router.delete("/{tweet_id}/likes", status_code=status.HTTP_200_OK)
+async def delete_like(
+    request: Request,
+    tweet_id: int,
+    tweet: Tweet = Depends(tweet_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    api_key = request.headers.get("Api-Key")
+    user = await users_crud.get_user_by_api_key(session=session, api_key=api_key)
+    return await crud.delete_like(session=session, tweet_id=tweet_id, user_id=user.id)
