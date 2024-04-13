6c5991642f8fdb416cd752b6674ddc30880dfa21
Nufore <werd322@yandex.ru>
2024-04-13 17:11:32 +0300

    документация по Users в dependencies

--- app/fastapi_app/users/dependencies.py
+++ app/fastapi_app/users/dependencies.py
@@ -12,6 +12,15 @@ async def user_by_id(
         user_id: Annotated[int, Path],
         session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ) -> User:
+    """
+    Получение данных пользователя по его id для зависимости во views
+
+    :param user_id: id пользователя
+    :param session: Асинхронная сессия
+
+    :return: возвращаем данные по пользователю
+    или сообщение о том, что такого пользователя нет
+    """
     user = await crud.get_user(session=session, user_id=user_id)
     if user:
         return user
@@ -26,6 +35,15 @@ async def user_by_apikey(
         api_key: Annotated[str, Header()],
         session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ) -> User:
+    """
+    Получение данных пользователя по переданному API-ключу для зависимости во views
+
+    :param api_key: API-ключ переданный в headers при запросе
+    :param session: Асинхронная сессия
+
+    :return: возвращаем данные по пользователю
+    или сообщение о том, что пользователя с таким ключом нет
+    """
     user = await crud.get_user_by_api_key(session=session, api_key=api_key)
     if user:
         return user
